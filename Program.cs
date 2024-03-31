fc8c389ab1c754b3affced7f62ea37c8d5ab04f9
MassiHadjloum <massihadjloum@gmail.com>
2024-03-31 13:44:26 +0200

    add time out pollicy & exponential backoff & circuit breaker

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@ using Play.Common.MongoDB;
 using Play.Common.Settings;
 using Play.Inventory.Service.Clients;
 using Play.Inventory.Service.Entities;
+using Polly;
+using Polly.Timeout;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -10,9 +12,36 @@ ServiceSettings serviceSettings = builder.Configuration.GetSection(nameof(Servic
 
 builder.Services.AddMongo().AddMongoRepository<InventoryItem>("inventoryItems");
 
+// add policy to specify timeout duration
 builder.Services.AddHttpClient<CatalogClinet>(client => {
     client.BaseAddress = new Uri("http://localhost:5046");
-});
+})
+.AddTransientHttpErrorPolicy(build => 
+    build.Or<TimeoutRejectedException>().WaitAndRetryAsync(5, 
+        retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+        onRetry: (outcome, timespan, retryAttempt) => {
+            var serviceProvider = builder.Services.BuildServiceProvider();
+            serviceProvider.GetService<ILogger<CatalogClinet>>()
+            ?.LogWarning($"Delaying for {timespan.TotalSeconds} seconds, then making retry {retryAttempt}");
+        }
+    )
+)
+.AddTransientHttpErrorPolicy(build => 
+    build.Or<TimeoutRejectedException>().CircuitBreakerAsync(
+        3, 
+        TimeSpan.FromSeconds(15), 
+        onBreak: (outcome, timespan) => {
+            var serviceProvider = builder.Services.BuildServiceProvider();
+                serviceProvider.GetService<ILogger<CatalogClinet>>()
+                ?.LogWarning($"Opening the circuit for {timespan.TotalSeconds} seconds ...");
+        },
+        onReset: () => {
+            var serviceProvider = builder.Services.BuildServiceProvider();
+                serviceProvider.GetService<ILogger<CatalogClinet>>()
+                ?.LogWarning($"Closing the circuit ...");
+        }
+    ))
+.AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(1));
 
 builder.Services.AddControllers(options =>
 {
